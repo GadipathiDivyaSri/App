3032c721e9be0df50caeebe41ee417accca08dac
gongidikalyan <gongidikalyang08@gmail.com>
2026-08-30 20:23:03 +0530

    fix(web): resolve javascript syntax error in main.dart.js to fix blank screen on load

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -100216,7 +100216,7 @@ n=A.X(k,B.P0,B.j,k,k,new A.O(g?B.X:B.N,k,k,k,k,k,B.b5),k,k,k,B.t_,k,k,k)
 l=A.m("Interactive Node Map",k,k,k,k,A.J(k,k,g?B.d:B.B,k,k,k,k,k,k,k,k,15,k,k,B.K,k,k,!0,k,k,k,k,k,k,k,k),k,k,k)
 l=A.aD(A.a0(A.a([l,B.ce,A.m("Drag, zoom & connect skill nodes live",k,k,k,k,A.J(k,k,g?B.as:B.a6,k,k,k,k,k,k,k,k,12,k,k,k,k,k,!0,k,k,k,k,k,k,k,k),k,k,k)],m),B.w,B.h,B.f,0,B.m),1)
 r=A.cc(k,A.X(k,A.a4(A.a([n,B.bx,l,A.as(B.NI,g?B.X:B.N,k,16)],m),B.i,B.h,B.f,0,k,k),B.j,k,k,new A.O(r,k,o,p,k,k,B.o),k,k,k,B.d2,k,k,1/0),B.t,!1,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,new A.a5G(a),k,k,k,k,k,k)
-r=A.a([q,B.aD,r,B.an,A.a4(A.a([A.m("",b,b,b,b,b,b,b,b),k,k,k),A.m(""+h.length+" Active",k,k,k,k,A.J(k,k,f,k,k,k,k,k,k,k,k,12,k,k,B.b4,k,k,!0,k,k,k,k,k,k,k,k),k,k,k)],m),B.i,B.ap,B.f,0,k,k),B.aP],m)
+r=A.a([q,B.aD,r,B.an,A.a4(A.a([A.m("Active Goals",k,k,k,k,A.J(k,k,g?B.d:B.B,k,k,k,k,k,k,k,k,18,k,k,B.K,k,k,!0,k,k,k,k,k,k,k,k),k,k,k),A.m(""+h.length+" Active",k,k,k,k,A.J(k,k,f,k,k,k,k,k,k,k,k,12,k,k,B.b4,k,k,!0,k,k,k,k,k,k,k,k),k,k,k)],m),B.i,B.ap,B.f,0,k,k),B.aP],m)
 q=h.length
 if(q===0)B.b.M(r,A.a([this.Gh(B.O3,g,0.6,"Pathways & Goal Goals","Make-up graduate"),B.a4,this.Gh(B.u2,g,0.4,"Draft your goals","Complete intention"),B.a4,this.Gh(B.NZ,g,0.25,"Contribution","Develop commissions")],m))
 else r.push(A.vP(new A.a5H(h,g),q,B.f2,!0))
