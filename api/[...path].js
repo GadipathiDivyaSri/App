4d254b1bf08ccb7e59cc94c8f03ec18421d51d6b
gongidikalyan <gongidikalyang08@gmail.com>
2026-09-13 16:30:37 +0530

    Fix Vercel catch-all serverless routing resolution for all endpoints

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,11 @@
 const { handleApiRequest } = require("../backend/api_handler");
 
 module.exports = async (req, res) => {
+  if (req.headers['x-invoke-path'] && req.headers['x-invoke-path'] !== '/api/[...path]') {
+    req.url = req.headers['x-invoke-path'];
+  } else if (req.query && req.query.path) {
+    const p = Array.isArray(req.query.path) ? req.query.path.join('/') : req.query.path;
+    req.url = '/api/' + p.replace(/^\/+/, '');
+  }
   return handleApiRequest(req, res);
 };
