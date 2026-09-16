25228440dccfe864d3db634416af675c1f2fa920
gongidikalyan <gongidikalyang08@gmail.com>
2026-09-16 21:49:47 +0530

    Fix Android CI build settings and provide release APK in docs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.wrindha.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
